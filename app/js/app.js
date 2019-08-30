09f0bef6609b6211a56db789952ebf841fffc186
wangyupeng <wangyupeng@hesipay.com>
2019-08-30 18:04:38 +0800

    页面开发（完工）

--- app/js/app.js
+++ app/js/app.js
@@ -4,10 +4,15 @@ window.onload = function (){
         scrollY: true
     })
     const mySwiper = new Swiper('.swiper-container', {
-        autoplay: true,//可选选项，自动滑动
+        autoplay: false,//可选选项，自动滑动
         pagination: {
             el: '.swiper-pagination',
             type: 'fraction'
         }
     })
+}
+
+function abc(ev) {
+    console.log(this)
+    return false
 }
